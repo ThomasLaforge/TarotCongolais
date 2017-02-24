91d13aa2427a62d915b15ed40adbbeab691f1686
Thomas Laforge <thomas.laforge.38@gmail.com>
2017-02-24 17:31:16 +0100

    default route

--- src/scripts/client/main.ts
+++ src/scripts/client/main.ts
@@ -5,8 +5,9 @@ const Foo = { template: '<div>foo</div>' }
 const Bar = { template: '<div>bar</div>' }
 
 const routes = [
-  { path: '/foo', component: Foo },
-  { path: '/bar', component: Bar }
+  { path: '/', component: Foo },
+  { path: '/login', component: Foo },
+  { path: '/board', component: Bar }
 ]
 
 const router = new VueRouter({
