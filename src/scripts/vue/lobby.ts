c801111e9036cba36d325b4e4fdcb77c2d8d8a91
Thomas Laforge <thomas.laforge.38@gmail.com>
2017-04-03 17:05:16 +0200

    improve lobby: game selection and syntax/interfaces

--- src/scripts/vue/lobby.ts
+++ src/scripts/vue/lobby.ts
@@ -1,21 +1,22 @@
 import { chat } from './chat'
-
+import { VueLobbyData, LobbyListElt } from '../modules/TarotCongolais'
 let template = `
 <div>
     <h2>Lobby</h2>
 
     <button @click="auto">Auto find game</button>
-    <button @click="update">Update list</button>
 
     <div class="game-list">
         <table>
             <thead>
-                <tr>name</th>
-                <tr>occupation</th>
+                <tr>
+                    <th>Name</th>
+                    <th>Occupation</th>
+                </tr>
             </thead>
             
             <tbody>
-                <tr class="game-list-elt" v-for="game in gameList" @click="selectGame(game.name)">
+                <tr class="game-list-elt " :class="selectedGameRoomId === game.name ? 'game-list-elt-selected' : 'game-list-elt-not-selected' " v-for="game in gameList" @click="selectGame(game.name)">
                     <td>{{ game.name }}</td>
                     <td>{{ game.playerOn }} / {{ game.maxPlayers }}</td>
                 </tr>
@@ -31,9 +32,9 @@ let template = `
 
 export const lobby = {
     template : template,
-    data: function(){
+    data: function() : VueLobbyData {
         return {
-            selectedGame: null,
+            selectedGameRoomId: null,
             gameList : [{
                 name : 'game 2',
                 maxPlayers : 4,
@@ -62,16 +63,21 @@ export const lobby = {
             this.$socket.emit('lobby-create')
         },
         join(){
-            if(this.selectedGame){
-                this.$socket.emit('lobby-join', this.selectedGame)
+            if(this.selectedGameRoomId){
+                this.$socket.emit('lobby-join', this.selectedGameRoomId)
             }
         },
         update(){
             this.$socket.emit('update_lobby_list');
         },
-        selectGame(game){
-            console.log('select game', game)
-            this.selectedGame = game
+        selectGame(gameRoomId: string){
+            console.log('select gameRoomId', gameRoomId)
+            if(this.selectedGameRoomId === gameRoomId){
+                this.selectedGameRoomId = null
+            }
+            else{
+                this.selectedGameRoomId = gameRoomId
+            }
         },
     },
     mounted: function(){
